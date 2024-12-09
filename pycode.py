bf02b471a60d3340cd222746a36adf09818f4ae1
username <you@example.com>
2024-12-09 08:41:42 +0300

    add new files

---
+++ pycode.py
@@ -0,0 +1,4 @@
+with open('UTF-8.txt', 'a', encoding='utf-8') as f_utf8:
+    f_utf8.write(', мир')
+with open('windows-1251.txt', 'a', encoding='windows-1251') as f_windows1251:
+    f_windows1251.write(', мир')
